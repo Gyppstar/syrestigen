b38b33c855b5716704175067e9a55c0b04edd7f2
aminsoudani <amin.soudani@hyperisland.se>
2025-05-23 11:11:48 +0200

    testade runt lite

--- Assets/Scripts/UImanager.cs
+++ Assets/Scripts/UImanager.cs
@@ -22,13 +22,5 @@ public class CanvasSwitcher : MonoBehaviour
 
         if (canvasB != null)
             canvasB.SetActive(true);
-
-        // Optional: Wait briefly to show canvasB before switching scene
-        yield return new WaitForSeconds(1f);
-
-        // Load the next scene in the build index
-        int currentIndex = SceneManager.GetActiveScene().buildIndex;
-        int nextIndex = (currentIndex + 1) % SceneManager.sceneCountInBuildSettings;
-        SceneManager.LoadScene(nextIndex);
     }
 }
